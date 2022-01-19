c51c473bd0993f1a6cac7ee954f741f835f70ffb
zeinadesouky <76789833+zeinadesouky@users.noreply.github.com>
2022-01-19 19:29:32 +0200

    Add files via upload

---
+++ code/medicine_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:health_care_app/Providers/meds.dart';
+import 'package:health_care_app/Screens/add_prescription.dart';
+import 'package:health_care_app/Widgets/medicines_view.dart';
+import 'package:health_care_app/Widgets/menu_bar.dart';
+import 'package:health_care_app/Widgets/navigation_bar.dart';
+import 'package:provider/provider.dart';
+import 'package:health_care_app/Widgets/add_medicine_dialog.dart';
+
+class MedicineList extends StatelessWidget {
+  const MedicineList({Key? key}) : super(key: key);
+  static const routeName = '/medicineList';
+
+  get menuBar => MenuBar();
+  get nav => Navigation();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Medicine List"),
+        backgroundColor: Color(0xff92C9D4),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: nav, //Navigation Bar
+      drawer: menuBar,
+      body: MedicinesWidget(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => AddMedicineDialog(),
+              fullscreenDialog: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
